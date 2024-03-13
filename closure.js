2a5e535f377cb6adb4eaf61dba483c7f2bb0000b
Aswathy <aswathyanuramar@gmail.com>
2024-03-13 14:39:01 +0530

    Closure Added

---
+++ closure.js
@@ -0,0 +1,14 @@
+function logger(type) {
+  let date = new Date().toUTCString();
+  return function (data) {
+    console.log(`${type}-${date}-${data}`);
+  };
+}
+
+const errorLogger = logger("ERROR");
+const debugLogger = logger("DEBUG");
+const infoLogger = logger("INFO");
+
+errorLogger("This is Error");
+debugLogger("This is Debug");
+infoLogger("This is Info");
